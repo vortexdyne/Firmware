c2599e7acafe2557ce88b134c6d86d853f40e807
Julian Oes <julian@oes.ch>
2016-06-06 13:55:18 +0200

    commander: only warn if termination happens
    The termination warnings were printed even if termination was not
    actually enabled. This was confusing and is therefore fixed.

--- src/modules/commander/state_machine_helper.h
+++ src/modules/commander/state_machine_helper.h
@@ -76,6 +76,7 @@ struct status_flags_s {
 	bool circuit_breaker_engaged_airspd_check;
 	bool circuit_breaker_engaged_enginefailure_check;
 	bool circuit_breaker_engaged_gpsfailure_check;
+	bool circuit_breaker_flight_termination_disabled;
 	bool cb_usb;
 	bool offboard_control_signal_found_once;
 	bool offboard_control_signal_lost;
